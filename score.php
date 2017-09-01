eb5fae190069830536c72df20dade4270c5ce415
James O <james@au044946>
2017-09-01 13:13:50 +1000

    server works now

--- score.php
+++ score.php
@@ -5,7 +5,8 @@ ini_set('display_errors', 1);
 
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-if(strlen($url)>0){
+//if($url["host"]){
+if(false){
 	$server = $url["host"];
 	$username = $url["user"];
 	$password = $url["pass"];
@@ -57,7 +58,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 else{	
 	// echo "get";
 	$myArray = array();
-	if ($result = $conn->query("SELECT id, name, max(score) as bestscore FROM scores group by name order by bestscore desc")) {
+	if ($result = $conn->query("SELECT name, max(score) as bestscore FROM scores group by name order by bestscore desc")) {
 
 	    while($row = $result->fetch_array(MYSQLI_ASSOC)) {
 	            $myArray[] = $row;
